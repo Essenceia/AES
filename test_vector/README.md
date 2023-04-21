a83e0dd75c1feee793c56cbf7f5272c631f9d010
Julia Desmazes <julia.desmazes@gmail.com>
2023-04-20 20:25:27 -0700

    TB : Added random generation of test vectors in bluk, updated readme with rundamentary usage

--- test_vector/README.md
+++ test_vector/README.md
@@ -1,11 +1,15 @@
 # Test vector generation
 
-Generate aes128 test vector, written in binary format
+Generate aes128 encode test vector, written in binary format
 
 To build : 
 ```
-make clean
 make 
+```
+
+To build in debug mode :
+```
+make debug=1
 ```
 
 To run :
@@ -20,3 +24,12 @@ Output will be written to files :
 - `aes_enc_key_i.txt`
 
 - `aes_enc_res_o.txt`
+
+
+## Configuration
+
+Generator behavior can be modified via macro's in `main.c`.
+
+- `TEST_NUM` number of test vectors to be generated and written to file, default value is `10`
+
+- `AES_SIZE` aes cypher size in bytes, default is `16`
--- test_vector/main.c
+++ test_vector/main.c
@@ -1,65 +1,60 @@
 #include <stdio.h>
 #include "aes.h"
 #include "file.h"
+#include "rand.h"
+
+// AES cypher size in bytes, aes128 by default
+#define AES_SIZE 16
+
+// number of test vectors to be generated
+#define TEST_NUM 10
+
 
 int main() {
 
-	uint8_t i;
-	uint8_t in[]= { 0x32 ,0x43 ,0xf6 ,0xa8 ,0x88 ,0x5a ,0x30 ,0x8d ,0x31 ,0x31 ,0x98 ,0xa2 ,0xe0 ,0x37 ,0x07 ,0x34};
-	uint8_t key []= { 0x2b ,0x7e ,0x15 ,0x16 ,0x28 ,0xae ,0xd2 ,0xa6 ,0xab ,0xf7 ,0x15 ,0x88 ,0x09 ,0xcf ,0x4f ,0x3c};
-
-	/* 128 bits */
-	/*
- * 	 uint8_t key[] = {
-		0x2b, 0x7e, 0x15, 0x16,
-		0x28, 0xae, 0xd2, 0xa6,
-		0xab, 0xf7, 0x15, 0x88,
-		0x09, 0xcf, 0x4f, 0x3c};
-
-	 uint8_t in[] = {
-		0x32, 0x43, 0xf6, 0xa8,
-		0x88, 0x5a, 0x30, 0x8d,
-		0x31, 0x31, 0x98, 0xa2,
-		0xe0, 0x37, 0x07, 0x34}; // 128
-	*/
-	
-	uint8_t out[16]; // 128
+	uint8_t i, j;
+	uint8_t  in[AES_SIZE];
+	uint8_t key[AES_SIZE];
+	uint8_t out[AES_SIZE];
 	uint8_t *w; // expanded key
 	tvf_s *f;
 		
 	w = aes_init(sizeof(key));
-	f= setup_files();
-
-	aes_key_expansion(key, w);
-
-	printf("Plaintext message:\n");
-	for (i = 0; i < 4; i++) {
-		printf("%02x %02x %02x %02x ", in[4*i+0], in[4*i+1], in[4*i+2], in[4*i+3]);
+	f = setup_files();
+	setup_rand();
+	
+	// generate multiple test vectors and write them to file	
+	for(j=0; j<TEST_NUM; j++){
+		// generate new random input and key
+		gen_rand(&in, AES_SIZE);
+		gen_rand(&key, AES_SIZE);
+		
+		aes_key_expansion(key, w);
+	
+		#ifdef DEBUG
+		printf("Plaintext message:\n");
+		for (i = 0; i < 4; i++) {
+			printf("%02x %02x %02x %02x ", in[4*i+0], in[4*i+1], in[4*i+2], in[4*i+3]);
+		}
+		printf("\n");
+		#endif
+		
+		aes_cipher(in /* in */, out /* out */, w /* expanded key */);
+		
+		#ifdef DEBUG
+		printf("Ciphered message:\n");
+		for (i = 0; i < 4; i++) {
+			printf("%02x %02x %02x %02x ", out[4*i+0], out[4*i+1], out[4*i+2], out[4*i+3]);
+		}
+		printf("\n");
+		#endif
+	
+		write_data8(f->f[0], &in,  sizeof(in));
+		write_data8(f->f[1], &key, sizeof(key));
+		write_data8(f->f[2], &out, sizeof(out));
+		
 	}
-
-	printf("\n");
-
-	aes_cipher(in /* in */, out /* out */, w /* expanded key */);
-
-	printf("Ciphered message:\n");
-	for (i = 0; i < 4; i++) {
-		printf("%02x %02x %02x %02x ", out[4*i+0], out[4*i+1], out[4*i+2], out[4*i+3]);
-	}
-
-	printf("\n");
-
-	aes_inv_cipher(out, in, w);
-
-	printf("Original message (after inv cipher):\n");
-	for (i = 0; i < 4; i++) {
-		printf("%02x %02x %02x %02x ", in[4*i+0], in[4*i+1], in[4*i+2], in[4*i+3]);
-	}
-
-	printf("\n");
-	write_data8(f->f[0], &in,  sizeof(in));
-	write_data8(f->f[1], &key, sizeof(key));
-	write_data8(f->f[2], &out, sizeof(out));
-
+	
 	free(w);
 	close_files(f);
 
